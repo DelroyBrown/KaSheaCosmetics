7054a9d546982b2a567adeabd8685b5b6c25671f
Delroy Brown <delroybrown.db@gmail.com>
2024-10-16 13:10:21 +0100

    Updated order admin and added read only fields

--- KaSheaCosmetics_orders/admin.py
+++ KaSheaCosmetics_orders/admin.py
@@ -5,12 +5,31 @@ from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    extra = 1
+    extra = 0
+    
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["id", "customer_name", "email", "amount", "status", "created_at"]
+    list_display = [
+        "id",
+        "customer_name",
+        "email",
+        "amount",
+        "status",
+        "created_at"
+        ]
     inlines = [OrderItemInline]
+    readonly_fields = [
+        'customer_name',
+        'email',
+        'shipping_address',
+        'shipping_city',
+        'shipping_postcode',
+        'shipping_country',
+        'amount',
+        'status',
+        'stripe_payment_intent_id',
+    ]
 
 
 admin.site.register(Order, OrderAdmin)
