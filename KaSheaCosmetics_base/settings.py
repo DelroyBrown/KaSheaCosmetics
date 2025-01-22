0f656d80c7995a567875a36d3cd2b194baf12af5
Delroy Brown <delroybrown.db@gmail.com>
2025-01-22 11:38:46 +0000

    Added AWS S3 storage configuration and updated .gitignore

--- KaSheaCosmetics_base/settings.py
+++ KaSheaCosmetics_base/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'storages',
     # My Apps
     "KaSheaCosmetics_base",
     "KaSheaCosmetics_home",
@@ -96,6 +97,31 @@ else:
 #         "postgres://ud5dd4fujmal6d:pc64ecb2a0e8793971cec5dbb4050b821cc5c5a0e9ee78e5fa40aaedb2f9f3059@clhtb6lu92mj2.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d6hd6qnpuvootm"
 #     )
 # }
+
+if "USE_AWS" in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+        "CacheControl": "max-age=999999999",
+    }
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = "eu-west-2"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_DEFAULT_ACL = None
+
+    # Static and media files
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = "static"
+    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = "media"
+
+    # Override static and media URLs in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
