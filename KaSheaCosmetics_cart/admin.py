7958f1fb7eddbf11ef90cfef2df8fa806094455a
Delroy Brown <delroybrown.db@gmail.com>
2025-01-27 13:26:32 +0000

    Updated admin.py to use unfold.admin instead of django.contrib.admin for ModelAdmin and StackedInline

--- KaSheaCosmetics_cart/admin.py
+++ KaSheaCosmetics_cart/admin.py
@@ -1,16 +1,16 @@
 # KaSheaCosmetics_cart\admin.py
 from django.contrib import admin
+from unfold.admin import ModelAdmin, StackedInline
 from .models import ShippingLocation
 from KaSheaCosmetics_products.models import DefaultShippingCost
 
-
 @admin.register(ShippingLocation)
-class ShippingLocationAdmin(admin.ModelAdmin):
+class ShippingLocationAdmin(ModelAdmin):
     list_display = ("city", "shipping_cost")
     search_fields = ("city",)
 
 
 @admin.register(DefaultShippingCost)
-class DefaultShippingCostAdmin(admin.ModelAdmin):
+class DefaultShippingCostAdmin(ModelAdmin):
     list_display = ("cost",)
     search_fields = ("cost",)
