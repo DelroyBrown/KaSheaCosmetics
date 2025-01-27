b011b661ea0e18bd00eb32ac53ce550413a969af
Delroy Brown <delroybrown.db@gmail.com>
2025-01-27 13:53:40 +0000

    updated settings

--- KaSheaCosmetics_base/settings.py
+++ KaSheaCosmetics_base/settings.py
@@ -174,6 +174,8 @@ STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
+
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
